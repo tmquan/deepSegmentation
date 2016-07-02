434209f636841737adc54c853389c48d8e05019d
tmquan <quantm@unist.ac.kr>
2016-07-02 22:47:28 +0900

    Initiate header TFlearn.py

---
+++ Model.py
@@ -0,0 +1,8 @@
+from TFlearn import *
+
+
+def network():
+	"""
+	Define the architecture of the network is here
+	"""
+def model():
---
+++ Utility.py
@@ -0,0 +1,5 @@
+import tflearn
+import numpy as np 
+
+if __name__ == '__main__':
+	pass
